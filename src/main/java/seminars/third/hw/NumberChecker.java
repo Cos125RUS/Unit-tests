ca5b52567c5750863a64492104978311a89c1051
Valerii_Panov <val250640@mail.ru>
2023-10-18 13:11:14 +1000

    hw03

---
+++ src/main/java/seminars/third/hw/NumberChecker.java
@@ -0,0 +1,11 @@
+package seminars.third.hw;
+
+public class NumberChecker {
+    public boolean isOdd(int num) {
+        return num%2 == 0;
+    }
+
+    public boolean inInterval(int num) {
+        return num <= 100 && num >= 25;
+    }
+}
